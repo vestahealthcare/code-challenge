11fd473345b2470fa284723879d7234d8dfb3384
rbaxter <rbaxter.08@gmail.com>
2019-12-12 15:56:58 -0500

    use demo project

--- next.config.js
+++ next.config.js
@@ -1,3 +1,20 @@
+const debug = process.env.NODE_ENV !== "production";
+
 module.exports = {
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/code-challenge' : '',
+  exportPathMap: function () {
+    return {
+      "/": { page: "/" },
+      "/about": { page: "/about" },
+    }
+  },
+  assetPrefix: !debug ? '/code-challenge/' : '',
+  webpack: (config, { dev }) => {
+    config.module.rules = config.module.rules.map(rule => {
+      if(rule.loader === 'babel-loader') {
+        rule.options.cacheDirectory = false
+      }
+      return rule
+    })
+    return config
+  }
 }
