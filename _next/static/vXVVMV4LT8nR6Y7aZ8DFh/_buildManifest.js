d4904ff64407323089b09f1423042f785e38e2e2
adrianveracom <1236638+adrianveracom@users.noreply.github.com>
2021-09-10 22:22:00 +0000

    Deploying to gh-pages from @ vestahealthcare/code-challenge@31596d80e6d1023fe2a2ace158836828d06b8bc8 🚀

--- _next/static/vXVVMV4LT8nR6Y7aZ8DFh/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-7229537a176cd9f975eb.js"],"/_error":["static/chunks/pages/_error-eb7f79f2af83e8ddf5e2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
